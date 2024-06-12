83c220a8e18005ed6d3464a96656b96be8612146
chinh <chinh3571113@gmail.com>
2024-06-12 12:35:30 +0700

    Create spinner loader component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer.js';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import ManageStaff from './components/ManageStaff/ManageStaff';
+import SpinnerLoader from './components/SpinnerLoader/SpinnerLoader.js';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [spinnerLoader, setSpinnerLoader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSpinnerLoader(false);
+    }, 2000)
+  }, [])
 
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home/> } />
-        <Route path='/manageStaff' element={<ManageStaff />} />
-      </Routes>
-      <Footer />
+      <div className='app' >
+        {
+          spinnerLoader ? (< SpinnerLoader />) : (
+            <>
+              < Header />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/manageStaff' element={<ManageStaff />} />
+              </Routes>
+              <Footer />
+            </>
+          )
+        }
+      </div>
     </>
   );
 }
---
+++ src/components/SpinnerLoader/SpinnerLoader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SpinnerLoader.css'
+import { Row } from 'react-bootstrap'
+
+export default function SpinnerLoader() {
+  return (
+    <>
+      <div className='spinnerLoader-component' >
+        <img src='assets/images/gif-7.gif' />
+      </div>
+    </>
+  )
+}
