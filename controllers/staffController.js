60ea31a874aee5c8a56c872769ff8bc0acf59d2e
chinh <chinh3571113@gmail.com>
2024-06-12 14:00:21 +0700

    create staff

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -4,7 +4,25 @@ const indexStaff = (req, res) => {
     res.send('<h1>This is staff page</h1>');;
 }
 
+// CREATE
+const createStaff = (req, res) => {
+    Staff.findOne({ id: req.body.id })
+        .then(result => {
+            if (result) res.json({ message: 0 });
+            else {
+                const newStaff = new Staff({
+                    id: req.body.id,
+                    name: req.body.name,
+                    phoneNumber: req.body.phoneNumber,
+                    role: req.body.role
+                });
 
+                newStaff.save()
+                    .then(result => res.json(result))
+                    .catch(err => console.log(err));
+            }
+        })
+}
 
 // READ
 const readAllStaff = (req, res) => {
@@ -17,11 +35,22 @@ const readAllStaff = (req, res) => {
         })
 }
 
+// DELETE
+const deleteOneStaff = (req, res) => {
+    const id = req.params.id;
+
+    Staff.deleteOne({ id: id })
+        .then((result) => {
+            res.redirect('/staffs');
+        })
+        .catch(err => console.log(err));
+}
 
 
 
 
 module.exports = {
     indexStaff,  
-    readAllStaff
+    readAllStaff,
+    deleteOneStaff
 }
--- routes/staffRouter.js
+++ routes/staffRouter.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 // Manage Staff
 router.get('/', staffController.indexStaff);
-
+router.post('/create', staffController.createStaff);
 router.get('/read', staffController.readAllStaff);
-
+router.delete('/delete/:id', staffController.deleteOneStaff);
 
 
 module.exports = router;
