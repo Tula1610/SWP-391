881a3248d2fa376e95c511bf94293ebf59355098
chinh <chinh3571113@gmail.com>
2024-06-13 07:28:15 +0700

    update staff

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -6,7 +6,8 @@ const indexStaff = (req, res) => {
 
 // CREATE
 const createStaff = (req, res) => {
-    Staff.findOne({ id: req.body.id })
+    let query = {id: req.body.id.toString() };
+    Staff.findOne(query)
         .then(result => {
             if (result) res.json({ message: 0 });
             else {
@@ -35,22 +36,65 @@ const readAllStaff = (req, res) => {
         })
 }
 
+const readOneStaff = (req, res) => {
+    const id = req.params.id;
+
+    Staff.findOne({ id: id })
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+
 // DELETE
 const deleteOneStaff = (req, res) => {
     const id = req.params.id;
 
     Staff.deleteOne({ id: id })
-        .then((result) => {
+        .then(() => {
             res.redirect('/staffs');
         })
         .catch(err => console.log(err));
 }
 
+// UPDATE
+const updateOneStaff = (req, res) => {
+    const oldId = req.body.oldId;
+    const newId = req.body.newId;
+    let query = {id: newId.toString()}
+    
+    Staff.findOne(query)
+        .then((result) => {
+            if (result && result.id !== oldId) res.json({ message: 0 });
+            else {
+                Staff.deleteOne({ id: req.body.oldId })
+                    .then(result => res.send(result))
+                    .catch(err => console.log(err));
 
+                const newStaff = new Staff({
+                    id: req.body.newId,
+                    name: req.body.name,
+                    phoneNumber: req.body.phoneNumber,
+                    role: req.body.role
+                });
+
+                newStaff.save()
+                    .then(result => res.json(result))
+                    .catch(err => console.log(err));
+            }
+        })
+        .catch(err => console.log(err));
+}
 
 
 module.exports = {
-    indexStaff,  
+    indexStaff,
     readAllStaff,
-    deleteOneStaff
+    readOneStaff,
+    deleteOneStaff,
+    createStaff,
+    updateOneStaff
 }
